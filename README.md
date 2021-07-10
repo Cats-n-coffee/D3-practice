81aae82191245163dd4f07885176b0fdbcf9e957
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-07-10 15:46:35 -0400

    added tooltip, resolved yscale, legend

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 This repository holds different practice project for D3.js.
 
-**Some datasets are copied from FreeCodeCamp.**
+**Datasets are copied from FreeCodeCamp.**
 
 ## Bar chart
+This bar chart shows the USA Gross Domestic Product from 1947-2015. The dataset has the GDP for every year, for the months of January, April, July and October. The chart shows the GDP for every year (average of all 4 data points in a given year).
 
 Structure:
 - Append `svg` (width, height)
@@ -20,7 +21,15 @@ The bars were overflowing from the group element that was being stretched. The b
 Solution: corrected the height of the bars by subtracting the y value from height of the chart.
 
 ## Scatterplot
+This scatterplot shows the 35 fastest times at Alpes d'Huez stage. It depicts the time and the year for each performance, and shows which performance/cyclist reported/was reported using drugs for this performance.
 The same structure as the previous chart applies.
 the legend was made by using a group (`g`) for the legend, and one `g` for each color with its text. Each `g` has a rectangle `rect` for the color, and a `text` element.
 
 To allow for more space for the dots at both end ranges on the X axis (year 1994 and 2015), we substracted 1 from 1994 and added 1 to 2015. By doing this,the dots corresponding to these two years are further from the edges of the chart, and those from 1994 are away from the Y axis.
+
+## Heatmap
+This heatmap shows the temperature at the surface of the planet (in-land) from 1753 - 2015. The base temperature of 8.66 Celcius is used. The map shows the variance from this base temperature, ranging from blue colors (colder) to orange/red colors (warmer). One could argue that according to this dataset, land-surface has gotten warmer over the years.
+The most challenging part was to figure out which scale to use.
+- X axis (years) --> scaleLinear (might have been able to use scaleTime?)
+- Y axis (months) --> scaleBand: use for data that is *ordinal* (aka order as in 1st, 2nd, 3rd ...) and *categorical* (example: months, days of the week, ...). 
+- Colors (variance) --> scaleQuantize: transforms continuous data to discrete data. We need the data to be split into chunks, that will put data into groups and associate a color with it.
--- heatmap-freecodecamp/script.js
+++ heatmap-freecodecamp/script.js
@@ -7,68 +7,124 @@ async function draw() {
     const yearAccessor = d => d.year;
     const monthAccessor = d => d.month;
     // Calculates the variance
-    const accessor = d => d.variance + baseTemp;
+    const accessor = d => d.variance;
 
     // Dimensions (see below draw function for calculations)
     const dimensions = {
         width: 1148,
-        height: 580,
+        height: 480,
         margin: 50
     }
 
     dimensions.containerWidth = dimensions.width - dimensions.margin * 2;
     dimensions.containerHeight = dimensions.height - dimensions.margin * 2;
 
-    let colorSchema = (d3.schemeRdYlBu[11]).reverse();
-
     // Scales
-    const colorScale = d3.scaleQuantize() // or do we need scaleQuantize?
-        .domain(d3.extent(dataset, accessor)) // --> Needs to maintain the original order of the set?
-        .range(colorSchema) 
+    let colorSchema = (d3.schemeRdYlBu[11]).reverse();
+    const colorScale = d3.scaleQuantize() // Creates 'buckets' and assigns a color
+        .domain(d3.extent(dataset, accessor)) 
+        .range(colorSchema); 
 
     const xScale = d3.scaleLinear()
         .domain(d3.extent(dataset, yearAccessor))
-        .range([0, dimensions.containerWidth])
+        .range([0, dimensions.containerWidth]);
 
-    const yScale = d3.scaleLinear()
-        .domain(d3.extent(dataset, monthAccessor))
-        .range([dimensions.containerHeight, 0])
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const yScale = d3.scaleBand() // Months are ordered (1, 2, ...12) and their own category (month)
+        .domain(months)
+        .range([0, dimensions.containerHeight]); // The range in this order (starting with 0) allows us to calculate the tooltip 'top' position in a simple manner
 
     // Draw the images
     const svg = d3.select('#chart')
         .append('svg')
         .attr('width', dimensions.width)
-        .attr('height', dimensions.height)
+        .attr('height', dimensions.height);
 
     const container = svg.append('g')
         .attr('transform', `translate(${dimensions.margin + 15}, ${dimensions.margin})`);
 
+    const tooltip = d3.select('#tooltip');
+
     container.selectAll('rect')
         .data(dataset)
         .join('rect')
-        .attr('width', 4)
-        .attr('height', 40)
-        .attr('x', d => xScale(yearAccessor(d))) // calculate the position of the box
-        .attr('y', d => yScale(monthAccessor(d)) - 40) // calculate the position of the box
-        .attr('fill', d => colorScale(accessor(d))) // remember to add the accessor for color scale as well
+        .attr('width', dimensions.containerWidth / (dataset.length / 12)) // Uses the container width and dataset length
+        .attr('height', dimensions.containerHeight / 12) // Uses the container height and 12 months
+        .attr('x', d => xScale(yearAccessor(d))) // Determines the element's position on the X axis using the xScale and the year of the element (yearAccessor)
+        .attr('y', d => yScale(months[d.month - 1])) // Determines the element's position on the Y axis using the yScale and the element's month from the array (using the index [d.month - 1])
+        .attr('fill', d => colorScale(accessor(d))) // Remember to add the accessor for color scale as well, to access the element's variance
+        .on('mouseenter', function(event, datum) {
+            tooltip.style('display', 'block')
+                .style('top', (datum.month * dimensions.containerHeight) / 12 + 70 + 'px') // Tooltip will always be on the same level above the hovered element
+                .style('left', xScale(yearAccessor(datum)) + 70 +'px')
 
+            tooltip.select('.tooltip-year span')
+                .text(datum.year)
+
+            tooltip.select('.tooltip-month span')
+                .text(months[datum.month - 1]) // Renders the month in plain English
+
+            tooltip.select('.tooltip-temp span')
+                .text((datum.variance + baseTemp).toFixed(2))
+
+            tooltip.select('.tooltip-variance span')
+                .text(datum.variance)
+        })
+        .on('mouseleave', function(event, datum) {
+            tooltip.style('display', 'none')
+        });
+
+    // Axes
     const xAxis = d3.axisBottom(xScale)
         .ticks(20)
-        .tickFormat(d => d)
+        .tickFormat(d => d); // Removes the ','
     const xAxisGroup = container.append('g')
         .call(xAxis)
-        .style('transform', `translateY(${dimensions.containerHeight}px)`)
+        .style('transform', `translateY(${dimensions.containerHeight}px)`);
+    xAxisGroup.append('text')
+        .attr('x', dimensions.containerWidth / 2)
+        .attr('y', dimensions.margin)
+        .attr('fill', 'black')
+        .text('Years')
+        .style('font-size', '1rem');
 
-    const months = Array(12).fill(1).map((val, index) => new Date().setMonth(index));
     const yAxis = d3.axisLeft(yScale)
-        .ticks(12)
-        
-        //.tickValues(months)
-        // .tickFormat(d3.timeFormat("%B"))
+        .ticks(12);
     const yAxisGroup = container.append('g')
-        .call(yAxis)
+        .call(yAxis);
+    yAxisGroup.append('text')
+        .attr('x', -dimensions.containerHeight / 2 + dimensions.margin)
+        .attr('y', -dimensions.margin)
+        .style('transform', 'rotate(270deg)')
+        .attr('fill', 'black')
+        .text('Months')
+        .style('font-size', '1rem');
 
-}// https://ashlynnpai.github.io/journal/2018/02/14/heatmap-with-d3-v4.html
+    // Legend
+    const colorThresholds = colorScale.thresholds();
+    console.log(colorThresholds)
+    const legend = svg.append('g')
+        .attr('transform', `translate(${dimensions.containerWidth / 2 - dimensions.margin * 3})`)
+    legend.selectAll('rect')
+        .data(colorSchema)
+        .join('rect') // Adds each color as a rectangle
+        .attr('x', (d, i) => 40 * i)
+        .attr('y', 0)
+        .attr('width', 40)
+        .attr('height', 20)
+        .attr('fill', d => d)
+    legend.selectAll('text') // Adds the text for each reactangle
+        .data(colorSchema)
+        .join('text')
+        .attr('x', (d, i) => 40 * i + 30)
+        .attr('y', 30)
+        .attr('fill', 'black')
+        .text((d, i) => {
+            const number = (colorThresholds[i] + baseTemp).toFixed(2)
+            return isNaN(number) ? '' : number;
+        })
+        .style('font-size', '0.7rem')
+}
 
 draw();
 
