043f1d716c1766a761398a2047c0e6fa469c8d88
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-07-15 23:42:31 -0400

    started scale

--- choropleth-freecodecamp/script.js
+++ choropleth-freecodecamp/script.js
@@ -3,6 +3,10 @@ async function draw() {
     const mapData = await d3.json('us-counties.json');
     const feature = topojson.feature(mapData, mapData.objects.counties)
 
+    // Education data
+    const educData = await d3.json('us-education.json');
+    const educAccessor = d => d.bachelorsOrHigher;
+
     // Containers dimensions
     const dimensions = {
         width: 1200,
@@ -12,6 +16,11 @@ async function draw() {
 
     dimensions.containerWidth = dimensions.width - dimensions.margin * 2;
     dimensions.containerHeight = dimensions.height - dimensions.margin * 2;
+
+    // Scale
+    const colorScale = d3.scaleQuantize()
+        .domain(d3.extent(educData, educAccessor))
+        .range(d3.schemeBlues[8])
 
     // Draw images
     const svg = d3.select('#chart')
@@ -23,15 +32,19 @@ async function draw() {
         .attr('transform', `translate(${dimensions.margin}, ${dimensions.margin})`)
 
     const path = d3.geoPath()
-    console.log(path(feature))
+
     container.selectAll('path')
         .data(feature.features)
         .join('path')
         .attr('d', path)
         //.attr('transform', 'scale(0.82, 0.62)')
         .attr('class', 'county')
+        .attr('data-id', d => d.id)
+        .data(educData)
 }
 
 draw();
 
 // https://github.com/borntofrappe/FreeCodeCamp-Choropleth-Map
+
+// https://stackoverflow.com/questions/17817849/d3-js-how-to-join-data-from-more-sources
